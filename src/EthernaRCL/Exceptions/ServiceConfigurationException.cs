583462b26b13ed21de6251662fc7e9fb0534f751
Mirko Da Corte <mirko@etherna.io>
2021-11-03 16:56:14 +0100

    Added ServiceConfigurationException

---
+++ src/EthernaRCL/Exceptions/ServiceConfigurationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Etherna.RCL.Exceptions
+{
+    public class ServiceConfigurationException : Exception
+    {
+        public ServiceConfigurationException()
+        {
+        }
+
+        public ServiceConfigurationException(string message) : base(message)
+        {
+        }
+
+        public ServiceConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
