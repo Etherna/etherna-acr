a2a029d3f81ce86c9a378e09b52b3f3be916938c
Mirko Da Corte <mirko@etherna.io>
2021-11-04 02:20:49 +0100

    Added user claims

---
+++ src/EthernaRCL/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Etherna.RCL.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string GetEtherAddress(this ClaimsPrincipal user)
+        {
+            if (user is null)
+                throw new ArgumentNullException(nameof(user));
+
+            var claim = user.Claims.First(claim => claim.Type == UserClaimTypes.EtherAddress);
+            return claim.Value;
+        }
+
+        public static string[] GetEtherPrevAddresses(this ClaimsPrincipal user)
+        {
+            if (user is null)
+                throw new ArgumentNullException(nameof(user));
+
+            var claim = user.Claims.First(claim => claim.Type == UserClaimTypes.EtherPreviousAddresses);
+            return JsonSerializer.Deserialize<string[]>(claim.Value) ?? Array.Empty<string>();
+        }
+    }
+}
---
+++ src/EthernaRCL/UserClaimTypes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Etherna.RCL
+{
+    public static class UserClaimTypes
+    {
+        public const string EtherAddress = "ether_address";
+        public const string EtherPreviousAddresses = "ether_prev_addresses";
+        public const string IsWeb3Account = "isWeb3Account";
+        public const string Role = ClaimTypes.Role;
+
+        public static readonly IEnumerable<string> Names =
+            new[] { EtherAddress, EtherPreviousAddresses, IsWeb3Account, Role };
+    }
+}
