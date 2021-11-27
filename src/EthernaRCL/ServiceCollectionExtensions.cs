15c0226911c401bd44d82bbc1eec5ac33067f581
Mirko Da Corte <mirko@etherna.io>
2021-11-27 18:04:36 +0100

    Added ServiceCollectionExtensions class

---
+++ src/EthernaRCL/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Etherna.RCL.Services;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddEthernaRCL(this IServiceCollection services)
+        {
+            // Register dependencies.
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            // Register services.
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddScoped<IRazorViewRenderer, RazorViewRenderer>();
+        }
+    }
+}
