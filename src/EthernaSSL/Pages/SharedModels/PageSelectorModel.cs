c355dd2840ac3dc5b2171717f50c083b5fb4b6b2
Mirko Da Corte <mirko@etherna.io>
2022-01-24 19:15:50 +0100

    Added parametric first page

--- src/EthernaSSL/Pages/SharedModels/PageSelectorModel.cs
+++ src/EthernaSSL/Pages/SharedModels/PageSelectorModel.cs
@@ -8,27 +8,31 @@ namespace Etherna.SSL.Pages.SharedModels
         /// <summary>
         /// Constructor for PageSelector partial view Model
         /// </summary>
-        /// <param name="currentPage">Current page, starting from 1</param>
+        /// <param name="currentPage">Current page</param>
         /// <param name="maxPage">Total page count</param>
+        /// <param name="firstPage">First page number. Usually it can be 0 or 1</param>
         /// <param name="pageParamName">Name of page query parameter</param>
         /// <param name="routeData">Additional data to route</param>
         public PageSelectorModel(
             int currentPage,
             int maxPage,
+            int firstPage = 0,
             string pageParamName = "p",
             Dictionary<string, string>? routeData = null)
         {
-            if (currentPage < 1)
-                throw new ArgumentOutOfRangeException(nameof(currentPage), "Value can't be less than 1");
-            if (maxPage < 1)
-                throw new ArgumentOutOfRangeException(nameof(maxPage), "Value can't be less than 1");
+            if (currentPage < firstPage)
+                throw new ArgumentOutOfRangeException(nameof(currentPage), $"Value can't be less than {firstPage}");
+            if (maxPage < firstPage)
+                throw new ArgumentOutOfRangeException(nameof(maxPage), $"Value can't be less than {firstPage}");
 
             CurrentPage = currentPage;
             MaxPage = maxPage;
+            FirstPage = firstPage;
             PageParamName = pageParamName ?? throw new ArgumentNullException(nameof(pageParamName));
             RouteData = routeData ?? new Dictionary<string, string>();
         }
 
+        public int FirstPage { get; }
         public int CurrentPage { get; }
         public int MaxPage { get; }
         public string PageParamName { get; }
